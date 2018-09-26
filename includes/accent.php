f012ba1a6b46d452f1d8c89adfba64532d764b95
Robert Bernier <robertbernier@Roberts-MacBook-Pro-2.local>
2018-09-26 13:59:57 -0400

    Add several core modules to project

---
+++ includes/accent.php
@@ -0,0 +1,16 @@
+<nav class='accent'>
+  <div class='accent__inner'>
+    <a href='#'>
+      <span>Panel 1</span>
+    </a>
+    <a href='#'>
+      <span>Panel 2</span>
+    </a>
+    <a href='#'>
+      <span>Panel 3</span>
+    </a>
+    <a href='#'>
+      <span>Panel 4</span>
+    </a>
+  </div>
+</nav>
---
+++ includes/work.php
@@ -0,0 +1,5 @@
+<div class='work'>
+  <div class='work__inner'>
+    
+  </div>
+</div>
