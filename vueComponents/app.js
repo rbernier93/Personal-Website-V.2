6d8d42006e520812e8b6c3ef1206d3b0bce3a0a2
Robert Bernier <robertbernier@Roberts-MacBook-Pro-2.local>
2018-11-11 23:36:13 -0500

    Add navigation interactive animation

--- vueComponents/app.js
+++ vueComponents/app.js
@@ -7,68 +7,109 @@ template: `
       <div class='bio page js-active'>
         <div class='page__inner'>
           <div class='page__left'>
-
+            <div class='bio__img'>
+              <picture>
+                  <source srcset="../img/bio-pic.png" media="(min-width: 1024px)" />
+                  <source srcset="../img/bio-pic.png" media="(min-width: 700px)" />
+                  <img srcset="../img/bio-pic.png" alt="Sample pic" />
+                </picture>
+              </div>
           </div>
           <div class='page__right'>
-
+            <h1>Here is an H1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis esse dignissimos enim numquam. Consectetur veritatis quam nesciunt tempore, voluptatem natus dolore odit maxime iusto culpa? Fugiat magnam quod voluptas nam.</p>
+            <h2>Here is an H2</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ut, quam inventore quisquam voluptatem magnam itaque dolores corrupti, reiciendis dolorum quae omnis id? Eveniet beatae quae sapiente cum porro culpa?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, totam. Consequuntur dicta asperiores doloremque voluptatum quas. Mollitia, numquam illum. Odit error perspiciatis iure ut amet aspernatur exercitationem quae, enim velit.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad facilis rem hic, distinctio deserunt sint reprehenderit voluptates adipisci, commodi, quidem magni delectus quis iste. Maxime accusamus quia eligendi aliquid deleniti?</p>
           </div>
         </div>
       </div>
 
-      <div class='bio page'>
+      <div class='page'>
         <div class='page__inner'>
           <div class='page__left'>
-
+              <h1>Here is an H1</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis esse dignissimos enim numquam. Consectetur veritatis quam nesciunt tempore, voluptatem natus dolore odit maxime iusto culpa? Fugiat magnam quod voluptas nam.</p>
+              <h2>Here is an H2</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ut, quam inventore quisquam voluptatem magnam itaque dolores corrupti, reiciendis dolorum quae omnis id? Eveniet beatae quae sapiente cum porro culpa?</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, totam. Consequuntur dicta asperiores doloremque voluptatum quas. Mollitia, numquam illum. Odit error perspiciatis iure ut amet aspernatur exercitationem quae, enim velit.</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad facilis rem hic, distinctio deserunt sint reprehenderit voluptates adipisci, commodi, quidem magni delectus quis iste. Maxime accusamus quia eligendi aliquid deleniti?</p>
           </div>
           <div class='page__right'>
-
+              <h1>Here is an H1</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis esse dignissimos enim numquam. Consectetur veritatis quam nesciunt tempore, voluptatem natus dolore odit maxime iusto culpa? Fugiat magnam quod voluptas nam.</p>
+              <h2>Here is an H2</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ut, quam inventore quisquam voluptatem magnam itaque dolores corrupti, reiciendis dolorum quae omnis id? Eveniet beatae quae sapiente cum porro culpa?</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, totam. Consequuntur dicta asperiores doloremque voluptatum quas. Mollitia, numquam illum. Odit error perspiciatis iure ut amet aspernatur exercitationem quae, enim velit.</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad facilis rem hic, distinctio deserunt sint reprehenderit voluptates adipisci, commodi, quidem magni delectus quis iste. Maxime accusamus quia eligendi aliquid deleniti?</p>
           </div>
         </div>
       </div>
 
-      <div class='bio page'>
+      <div class='page'>
         <div class='page__inner'>
           <div class='page__left'>
-
+              <h1>Here is an H1</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis esse dignissimos enim numquam. Consectetur veritatis quam nesciunt tempore, voluptatem natus dolore odit maxime iusto culpa? Fugiat magnam quod voluptas nam.</p>
+              <h2>Here is an H2</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ut, quam inventore quisquam voluptatem magnam itaque dolores corrupti, reiciendis dolorum quae omnis id? Eveniet beatae quae sapiente cum porro culpa?</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, totam. Consequuntur dicta asperiores doloremque voluptatum quas. Mollitia, numquam illum. Odit error perspiciatis iure ut amet aspernatur exercitationem quae, enim velit.</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad facilis rem hic, distinctio deserunt sint reprehenderit voluptates adipisci, commodi, quidem magni delectus quis iste. Maxime accusamus quia eligendi aliquid deleniti?</p>
           </div>
           <div class='page__right'>
-
+              <h1>Here is an H1</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis esse dignissimos enim numquam. Consectetur veritatis quam nesciunt tempore, voluptatem natus dolore odit maxime iusto culpa? Fugiat magnam quod voluptas nam.</p>
+              <h2>Here is an H2</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ut, quam inventore quisquam voluptatem magnam itaque dolores corrupti, reiciendis dolorum quae omnis id? Eveniet beatae quae sapiente cum porro culpa?</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, totam. Consequuntur dicta asperiores doloremque voluptatum quas. Mollitia, numquam illum. Odit error perspiciatis iure ut amet aspernatur exercitationem quae, enim velit.</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad facilis rem hic, distinctio deserunt sint reprehenderit voluptates adipisci, commodi, quidem magni delectus quis iste. Maxime accusamus quia eligendi aliquid deleniti?</p>
           </div>
         </div>
       </div>
 
-      <div class='bio page'>
+      <div class='page'>
         <div class='page__inner'>
           <div class='page__left'>
-
+              <h1>Here is an H1</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis esse dignissimos enim numquam. Consectetur veritatis quam nesciunt tempore, voluptatem natus dolore odit maxime iusto culpa? Fugiat magnam quod voluptas nam.</p>
+              <h2>Here is an H2</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ut, quam inventore quisquam voluptatem magnam itaque dolores corrupti, reiciendis dolorum quae omnis id? Eveniet beatae quae sapiente cum porro culpa?</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, totam. Consequuntur dicta asperiores doloremque voluptatum quas. Mollitia, numquam illum. Odit error perspiciatis iure ut amet aspernatur exercitationem quae, enim velit.</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad facilis rem hic, distinctio deserunt sint reprehenderit voluptates adipisci, commodi, quidem magni delectus quis iste. Maxime accusamus quia eligendi aliquid deleniti?</p>
           </div>
           <div class='page__right'>
-
+              <h1>Here is an H1</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis esse dignissimos enim numquam. Consectetur veritatis quam nesciunt tempore, voluptatem natus dolore odit maxime iusto culpa? Fugiat magnam quod voluptas nam.</p>
+              <h2>Here is an H2</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ut, quam inventore quisquam voluptatem magnam itaque dolores corrupti, reiciendis dolorum quae omnis id? Eveniet beatae quae sapiente cum porro culpa?</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, totam. Consequuntur dicta asperiores doloremque voluptatum quas. Mollitia, numquam illum. Odit error perspiciatis iure ut amet aspernatur exercitationem quae, enim velit.</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad facilis rem hic, distinctio deserunt sint reprehenderit voluptates adipisci, commodi, quidem magni delectus quis iste. Maxime accusamus quia eligendi aliquid deleniti?</p>
           </div>
         </div>
       </div>
     </div>
 
     <nav class='main-nav'>
-      <ul>
+      <ul class='nav__list'>
         <li class='nav__item js-active'>
           <a href='#' @click='changePage'>
-            <span>Page 01</span>
+            <span>Bio</span>
           </a>
         </li>
         <li class='nav__item'>
           <a href='#' @click='changePage'>
-            <span>Page 02</span>
+            <span>Work</span>
           </a>
         </li>
         <li class='nav__item'>
           <a href='#' @click='changePage'>
-            <span>Page 03</span>
+            <span>Hobbies + Interests</span>
           </a>
         </li>
         <li class='nav__item'>
           <a href='#' @click='changePage'>
-            <span>Page 04</span>
+            <span>Contact</span>
           </a>
         </li>
       </ul>
@@ -77,7 +118,7 @@ template: `
 </div>
 `,
 data: {
-    speed: 600,
+    speed: 700,
     animating: false,
   },
 
@@ -112,6 +153,12 @@ data: {
             $(e.target).parent().addClass('js-active');
             $this.animating = false;
           }, this.speed);
+      } else if ($(e.target).parent().hasClass('js-active')){
+        $(e.target).parent().addClass('shake-little');
+
+        setTimeout(function() {
+          $(e.target).parent().removeClass('shake-little');
+        }, this.speed / 2);
       }
 
       return false;
